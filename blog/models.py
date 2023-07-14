79148432cce73f60fe37cc715053a11edef02ca8
Rafe <stefanorafe@gmail.com>
2023-07-14 02:53:36 +0200

    adds models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', default='default.png', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(blank=True)
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+    )
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1, default='d')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    commentor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.blog.title} - {self.commentor.username}"
+
+
+class PostViewRecords(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    viewer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.blog.title} - {self.viewer.username}"
+
+
+class Like(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    liker = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.blog.title} - {self.liker.username}"
