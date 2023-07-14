5a10e8e7e71d51fa09fbcf49d7fe6449ff2b6205
Rafe <stefanorafe@gmail.com>
2023-07-14 03:46:46 +0200

    adds serializers

---
+++ blog/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import (
+    Blog,
+    Comment
+)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'commentor',
+            'created',
+            'content',
+            'blog'
+        )
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = (
+            'id',
+            'title',
+            'created',
+            'updated'
+            'content',
+            'image',
+            'slug',
+            'status',
+            'category',
+            'author',
+            'view_count',
+            'like_count',
+            'comment_count',
+            'comments',
+        )
